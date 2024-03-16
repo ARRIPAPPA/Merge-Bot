61d0f533441b609251e730bdb47927a8b3fe4b56
ARRIPAPPA <137713659+ARRIPAPPA@users.noreply.github.com>
2024-03-16 20:55:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     DATABASE_URL = ("mongodb+srv://animesaga:animesagas@cluster0.lkpnvsw.mongodb.net/?retryWrites=true&w=majority")
     LOGCHANNEL = ("-1001736029601")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BAF4f7kACJSU0Rqn2xWUz5NR9oTLraTXyqCgmLq1o9mF1R7AhxWdo1nJI38UO4rCTwKQ4Z4AI2Ys3Js4Sa9NrsG51V4YFog0BeUa-B2-Q-BWGlIl93NgUV1o10finQ8RplfsCgZOSvlK30wSOLEv9auC95oLUafmwWMNnckHgYVCWfxGMIrqM-WaXd9sJ67CMAWsK1x0NCvUC8IMMcdaBJQagaCxafG0RB1pusOVbx0Sq6XHNpJ59OBeIQqWpjl75Dza5gxFVftMsdnDpBEhjE-7r1X1SiSQP4GHYap7z_2dnpOKJeP0JpeNSF9xvV9gsdyhaTofkznfTSfuunnWAXJhankJhQAAAAFwDOYhAA")
+    USER_SESSION_STRING = ("BAF4f7kACJSU0Rqn2xWUz5NR9oTLraTXyqCgmLq1o9mF1R7AhxWdo1nJI38UO4rCTwKQ4Z4AI2Ys3Js4Sa9NrsG51V4YFog0BeUa-B2-Q-BWGlIl93NgUV1o10finQ8RplfsCgZOSvlK30wSOLEv9auC95oLUafmwWMNnckHgYVCWfxGMIrqM-WaXd9sJ67CMAWsK1x0NCvUC8IMMcdaBJQagaCxafG0RB1pusOVbx0Sq6XHNpJ59OBeIQqWpjl75Dza5gxFVftMsdnDpBEhjE-7r1X1SiSQP4GHYap7z_2dnpOKJeP0JpeNSF9xvV9gsdyhaTofkznfTSfuunnWAXJhankJhQAAAAFwDOYhAA")
     IS_PREMIUM = True
     START_PIC = ("https://graph.org/file/6b56e482d9865a49ed578.jpg")
     MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
